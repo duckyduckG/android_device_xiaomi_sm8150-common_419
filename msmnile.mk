6a4623f9844db51a1e533150d51992a10f4ff871
duckyduckG <duckyduckg65@gmail.com>
2025-10-25 23:21:07 +0500

    sm8150-common: initial uclamp configuration
    * taken from xiaomi sm8350
    
    Change-Id: I85b6b215cfc400b1f0ee4e5af20ed09ff593be1f

--- msmnile.mk
+++ msmnile.mk
@@ -306,6 +306,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
