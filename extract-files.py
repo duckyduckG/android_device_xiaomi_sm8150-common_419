00243f97dfaebfc0c5ed653890d61ef686cb2c60
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    sm8150-common: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -31,6 +31,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('<HID>0</HID>', '<HID>1</HID>'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/libsnapdragoncolor-manager.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 
