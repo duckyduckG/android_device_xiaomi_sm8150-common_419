a9d172ae124f1e92c865bd343d2758fc56423ed0
duckyduckG <duckyduckg65@gmail.com>
2026-04-06 23:10:09 +0500

    sm8150-common: Import snapdragon color manager
    Change-Id: I90a1c0614a736976d110f9cb854ac55d402f2545

--- msmnile.mk
+++ msmnile.mk
@@ -171,6 +171,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
+PRODUCT_COPY_FILES += \
+    hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
